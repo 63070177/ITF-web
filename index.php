39c7a31f0263c9d367aa8e0d158fb7f50c56cc95
63070177 <73645471+63070177@users.noreply.github.com>
2020-10-30 15:17:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,4 @@
-<html>
-    <head>
-        <title>Garthy</title>
-    </head>
-    <body>
-        <h1>My name: Supichaya Thamsuna</h1>
-        <h2>My nickname: Guitar</h2>
-        <p>Cat and Dora</p>
-        <img src="cat.jpg">
-        <br>
-        <img src="cat0.jpg">
-        <br>
-        <a href="https://www.dek-d.com/">My fav web</a>
-
-        <h2>    Joke Corner</h2>
-        <p></p>
-        <br>
-        <p></p>
-    </body>
-</html>
+<?php
+echo "<h2>ITAKMITL</h>";
+echo "Hello IT<br>";
+?>
