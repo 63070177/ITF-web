f16a94a727b582c3783839fb7d076c3d06b424f8
63070177 <73645471+63070177@users.noreply.github.com>
2020-10-30 15:06:22 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,13 +5,16 @@
     <body>
         <h1>My name: Supichaya Thamsuna</h1>
         <h2>My nickname: Guitar</h2>
-        <p>Cat and Dora</p>
+        <p>     Cat and Dora</p>
         <img src="cat.jpg">
+        <br>
         <img src=""cartoon-7.jpg>
         <br>
         <a href="https://www.dek-d.com/">My fav web</a>
 
-        <h2>Joke Corner</h2>
-        <p>งงตรงไหน</p>
+        <h2>    Joke Corner</h2>
+        <p></p>
+        <br>
+        <p></p>
     </body>
 </html>
