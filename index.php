c37fe08e4f2b30b026b7e3261635f60bc244445e
63070177 <73645471+63070177@users.noreply.github.com>
2020-10-30 15:26:54 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,13 @@
     <head>
         <title>Garthy</title>
     </head>
+    <head>
+        <meta charset="UTF-8">
+    </head>
     <body>
         <h1>My name: Supichaya Thamsuna</h1>
         <h2>My nickname: Guitar</h2>
-        <p>Cat and Dora</p>
+        <p>Cute Cat</p>
         <img src="cat.jpg">
         <br>
         <img src="cat0.jpg">
@@ -13,8 +16,8 @@
         <a href="https://www.dek-d.com/">My fav web</a>
 
         <h2>    Joke Corner</h2>
-        <p></p>
+        <p>ยามไหนที่ไม่มีใครเข้าใจ</p>
         <br>
-        <p></p>
+        <p>อยากให้รู้ไว้ ยามนั้น คือ รปภ.</p>
     </body>
 </html>
