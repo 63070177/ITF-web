7a1e2273300269342f62a9856032c2f60c9b08a6
63070177 <73645471+63070177@users.noreply.github.com>
2020-11-15 14:43:18 +0700

    Update table.php

--- table.php
+++ table.php
@@ -1,6 +1,10 @@
 <html>
 <head>
 <title>ITF Lab</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
 <body>
 <?php
@@ -25,6 +29,7 @@ while($Result = mysqli_fetch_array($res))
   <tr>
     <td><?php echo $Result['Name'];?></div></td>
     <td><?php echo $Result['Comment'];?></td>
+    <td><button type="button" class="btn btn-outline-danger">ลบ</button></td>
   </tr>
 <?php
 }
