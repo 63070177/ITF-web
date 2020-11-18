d001f98025cd0edb5780dad021b47c568363f93b
63070177 <73645471+63070177@users.noreply.github.com>
2020-11-18 22:24:01 +0700

    Update and rename delete.php to deleteform.php

---
+++ deleteform.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Delete Form</title>
+    </head>
+    <body>
+        <form action="delete.php" method="post" id="CommentForm">
+            Name:<br>
+            <input type="text" name="name" id="idName" placeholder="Enter Name"><br>
+            Comment:<br>
+            <textarea row="10" cols="20" name="comment" id="idComment" placeholder="Enter Comment"></textarea>
+            <input type="submit" id="commentBtn">
+        </form>
+   </body>
+</html>
