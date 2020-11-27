21f6906bdf0e025b1d22a20dbc7a806ecc999b7d
63070177 <73645471+63070177@users.noreply.github.com>
2020-11-27 14:21:31 +0700

    Create lab_insert.php

---
+++ lab_insert.php
@@ -0,0 +1,27 @@
+<?php
+
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'garthy.mysql.database.azure.com', 'it63070177@garthy', 'JSRhsq44', 'itflab', 3306);
+if (mysqli_connect_errno($conn))
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+
+
+$Product = $_POST['Product'];
+$Price = $_POST['Price'];
+$Discount = $_POST['Discount'];
+$Total = $_POST['Total'];
+
+
+$sql = "INSERT INTO guestbook (Product , Price , Discount , Total) VALUES ('$Product', '$Price', '$Discount' , '$Total')";
+
+
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+  
+mysqli_close($conn);
+?>
